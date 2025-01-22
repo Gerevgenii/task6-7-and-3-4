df6ff8c668de5e6e5540d57670fc1d824ec31291
Gerasimov Evgeniy Olegovich <367152@niuitmo.ru>
2025-01-22 17:53:27 +0300

    Add files via upload
    Hw6 Fixed

--- task2/index.js
+++ task2/index.js
@@ -1,61 +1,47 @@
+const checkbox1 = document.querySelectorAll('.checkbox');
+const subDropdowns = document.querySelectorAll('.sub-dropdown');
 
-const a = document.querySelectorAll('.turn-on')
-
-let safeIndex = -1
-
-for (let i = 0; i < a.length; i++) {
-  a[i].addEventListener("click", () => {
-    const str = "pressed";
-    if (a[i].classList.contains("pressed")) {
-      a[i].classList.remove("pressed")
-      a[i].classList.add("unpressed");
-    } else {
-      if (safeIndex !== -1) {
-        a[safeIndex].classList.remove("pressed");
-        a[safeIndex].classList.remove("unpressed");
-      }
-      a[i].classList.add("pressed")
-      a[i].classList.remove("unpressed");
-      safeIndex = i;
-    }
-  })
-}
-
-
-
-const checkbox1 = document.querySelectorAll(':is(.checkbox1, .checkbox2, .checkbox3, .checkbox4)')
-
-let safeIndex1 = -1
+let activeSubDrop = -1;
 
 for (let i = 0; i < checkbox1.length; i++) {
   checkbox1[i].addEventListener("click", () => {
-    if (checkbox1[i].classList.contains("pressed")) {
-      checkbox1[i].classList.remove("pressed")
+    if (subDropdowns[i].classList.contains("show")) {
+      subDropdowns[i].classList.remove("show");
+      activeSubDrop = -1;
     } else {
-      if (safeIndex1 !== -1) {
-        checkbox1[safeIndex1].classList.remove("pressed");
+      if (activeSubDrop !== -1) {
+        subDropdowns[activeSubDrop].classList.remove("show");
       }
-      checkbox1[i].classList.add("pressed")
-      safeIndex1 = i;
+      subDropdowns[i].classList.add("show");
+      activeSubDrop = i;
     }
   })
 }
 
-const checkbox2 = document.querySelectorAll(':is(.checkbox5, .checkbox6, .checkbox7, .checkbox8)')
+const turnOnes = document.querySelectorAll('.turn-on')
+const dropdowns = document.querySelectorAll('.dropdown')
 
-let safeIndex2 = -1
+let activeDrop = -1
 
-for (let i = 0; i < checkbox2.length; i++) {
-  checkbox2[i].addEventListener("click", () => {
-    const str = "pressed";
-    if (checkbox2[i].classList.contains("pressed")) {
-      checkbox2[i].classList.remove("pressed")
-    } else {
-      if (safeIndex2 !== -1) {
-        checkbox2[safeIndex2].classList.remove("pressed");
+for (let i = 0; i < turnOnes.length; i++) {
+  turnOnes[i].addEventListener("click", () => {
+    if (dropdowns[i].classList.contains("show")) {
+      dropdowns[i].classList.remove("show");
+      if (activeSubDrop !== -1) {
+        subDropdowns[activeSubDrop].classList.remove("show");
+        activeSubDrop = -1
       }
-      checkbox2[i].classList.add("pressed")
-      safeIndex2 = i;
+    } else {
+      if (activeDrop !== -1) {
+        dropdowns[activeDrop].classList.remove("show");
+
+      }
+      if (activeSubDrop !== -1) {
+        subDropdowns[activeSubDrop].classList.remove("show");
+        activeSubDrop = -1
+      }
+      dropdowns[i].classList.add("show")
+      activeDrop = i;
     }
   })
 }
